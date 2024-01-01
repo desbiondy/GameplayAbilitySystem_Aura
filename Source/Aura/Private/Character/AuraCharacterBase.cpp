2a5e6d2a3ea1d85a28dc1a9f4149b4c274c66f64
desbiondy <leodesbiondy@gmail.com>
2024-01-01 15:50:52 -0500

    Updated CharacterBase, New Characters directories

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Copyright Pamor
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
